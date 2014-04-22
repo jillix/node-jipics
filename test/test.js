9ae9d1fd40622867d339e11386f9bc407ce18d7a
Ionică Bizău <bizauionica@yahoo.com>
2014-04-22 17:57:36 +0300

    Get absolute path to the test file

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 var Jipics = require ("../index");
 
-Jipics.upload ("./heisencat.png", function (err, data) {
+Jipics.upload (__dirname + "/heisencat.png", function (err, data) {
     console.log (err || "Image sucessfully uploaded: " + JSON.stringify(data, null, 4));
 });
