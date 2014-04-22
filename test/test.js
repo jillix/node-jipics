182d6a07fd157ee859d28cabca2df1e23edcb622
Ionică Bizău <bizauionica@yahoo.com>
2014-04-22 17:50:49 +0300

    Stringify options

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 var Jipics = require ("../index");
 
 Jipics.upload ("./heisencat.png", function (err, data) {
-    console.log (err || "Image sucessfully uploaded: " + JSON.stringify(data));
+    console.log (err || "Image sucessfully uploaded: " + JSON.stringify(data, null, 4));
 });
