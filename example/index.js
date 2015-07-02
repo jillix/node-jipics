39508b7323101b8e59ac539a5c1dc0cb63431b2b
Ionică Bizău <bizauionica@yahoo.com>
2015-07-02 14:05:02 +0300

    An example of uploading a big image

--- example/index.js
+++ example/index.js
@@ -1,7 +1,16 @@
 // Dependencies
-var Jipics = require("../lib");
+var Jipics = require("../lib")
+  , Request = require("request")
+  ;
 
 // Upload the local file
 Jipics.upload(__dirname + "/heisencat.png", function (err, data) {
+    console.log(err || "Image sucessfully uploaded: " + JSON.stringify(data, null, 4));
+});
+
+// Upload some laaaarge file, from remote
+Jipics.upload({
+    stream: Request("https://github.com/IonicaBizau/node-statique/raw/master/examples/public/images/large.jpg")
+}, function (err, data) {
     console.log(err || "Image sucessfully uploaded: " + JSON.stringify(data, null, 4));
 });
